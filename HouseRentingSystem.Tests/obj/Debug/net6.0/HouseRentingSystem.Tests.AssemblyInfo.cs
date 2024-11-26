b8c5e9e267c3e0ec15cf82e452afedce809863cc
Boris Penchev <tzars1@gmail.com>
2024-11-26 22:31:41 +0200

    Test

--- HouseRentingSystem.Tests/obj/Debug/net6.0/HouseRentingSystem.Tests.AssemblyInfo.cs
+++ HouseRentingSystem.Tests/obj/Debug/net6.0/HouseRentingSystem.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRentingSystem.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b3f756659c6f2d8a0bcdacc36298a968cefc43")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRentingSystem.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRentingSystem.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
